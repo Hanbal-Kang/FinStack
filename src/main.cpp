5ae6bc57c9a016d4ea62ad1dab152c2643871136
Hanbal-Kang <kang2005hanbal@gmail.com>
2026-04-18 21:50:13 +0500

    Added app.qss Globally in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
+#include <QFile>
 #include "ui/LoginWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName("FinStack");
-    app.setOrganizationName("FinStack");
+
+    QFile styleFile(":/styles/app.qss");
+    if (styleFile.open(QFile::ReadOnly))
+    {
+        app.setStyleSheet(styleFile.readAll());
+        styleFile.close();
+    }
 
     LoginWindow window;
     window.show();

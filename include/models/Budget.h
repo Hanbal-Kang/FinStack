b6247a794ad3b99f352522b9007f0b630af5282a
Hanbal-Kang <kang2005hanbal@gmail.com>
2026-04-24 14:55:18 +0500

    Added Budget.h

---
+++ include/models/Budget.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <QString>
+
+class Budget
+{
+private:
+    int id;
+    int user_id;
+
+    //Category for which we need to set a budget (e.g Food ,transport etc)
+    QString category;
+
+    //The budget amount set for that specific category
+    double monthly_limit;
+public:
+    Budget();
+
+    //Getters
+    int get_id() const;
+    int get_user_id() const;
+    QString get_category() const;
+    double get_monthly_limit() const;
+
+    //Setters
+    void set_id(int id);
+    void set_user_id(int id);
+    void set_category(const QString& category);
+    void set_monthly_limit(double amount);
+
+    //Check the validty
+    bool isValid() const;
+};

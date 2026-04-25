499707e877f7bef79ae2b3219806ca027e7cda04
Muhammad-Abdullah-Hussain <ag1954138@gmail.com>
2026-04-25 14:27:20 +0500

    update AuthService.h and AuthService.cpp
    created auth service

---
+++ src/services/AuthService.cpp
@@ -0,0 +1,43 @@
+#include "services\AuthService.h"
+#include "database\DatabaseManager.h"
+
+#include <QSqlQuery>
+#include <QVariant>
+#include <QCryptographicHash>
+
+QString AuthService::hashPassword(const QString& password) {
+    QByteArray hash = QCryptographicHash::hash(
+        password.toUtf8(),
+        QCryptographicHash::Sha256
+        );
+    return QString(hash.toHex());
+}
+
+bool AuthService::registerUser(const QString& username,
+                               const QString& password,
+                               const QString& email) {
+    QSqlQuery q = DatabaseManager::instance().prepare(
+        "INSERT INTO users(username, password_hash, email) VALUES (?, ?, ?)"
+        );
+
+    q.addBindValue(username);
+    q.addBindValue(hashPassword(password));
+    q.addBindValue(email);
+
+    return q.exec();
+}
+
+bool AuthService::login(const QString& username,
+                        const QString& password) {
+    QSqlQuery q = DatabaseManager::instance().prepare(
+        "SELECT password_hash FROM users WHERE username = ?"
+        );
+
+    q.addBindValue(username);
+
+    if (!q.exec() || !q.next())
+        return false;
+
+    QString storedHash = q.value(0).toString();
+    return storedHash == hashPassword(password);
+}

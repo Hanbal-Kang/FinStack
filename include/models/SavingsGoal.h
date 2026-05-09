69941f6a74d54f768b98c2d48a7a5c47053b1df1
slimshadycodess <maryam.waseem505@gmail.com>
2026-05-09 22:29:38 +0500

    goal model

--- include/models/SavingsGoal.h
+++ include/models/SavingsGoal.h
@@ -2,39 +2,33 @@
 #include <QString>
 #include <QDateTime>
 
-class SavingsGoal
-{
+class SavingsGoal {
 private:
-
     int savings_goal_id;
     int user_id;
-
-    //The amount which the user will set as goal to save
     double target_amount;
-
-    //The date which the user will as set as till when he has to complete this saving goal
+    double saved_amount; // Added to track progress
     QDateTime deadline;
-
-    //Note like for what he is saving this(e.g Buy new Laptop) (Optional)
     QString description;
-public:
 
+public:
     SavingsGoal();
 
-    //Getters
+    // Getters
     int get_savings_goal_id() const;
     int get_user_id() const;
     double get_target_amount() const;
+    double get_saved_amount() const;
     QDateTime get_deadline() const;
     QString get_description() const;
 
-    //Setters
+    // Setters
     void set_savings_goal_id(int id);
     void set_user_id(int id);
     void set_target_amount(double amount);
+    void set_saved_amount(double amount);
     void set_deadline(const QDateTime& deadline);
     void set_description(const QString& description);
 
-    //Validty
     bool isValid() const;
 };
